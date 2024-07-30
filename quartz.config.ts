11dc4c5afd15c435cc482bd2f4067d639f89a9df
Raphael Lee <raphaellee.ks@ebsl.hk>
2024-07-30 21:38:27 +0800

    Quartz sync: Jul 30, 2024, 9:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "life.rephaellee.dev",
+    baseUrl: "life.ralee.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
